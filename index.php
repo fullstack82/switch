a89a5b2516fbd5578f9caea39f560fe09cc82711
fullstack82 <fullstack8210@gmail.com>
2021-01-28 23:25:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+
+// Switch
+
+$dia = 6;
+
+switch ($dia) {
+    case 1:
+        echo "es lunes";
+        break;
+
+    case 2:
+        echo "es martes";
+        break;
+
+    case 3:
+        echo "es miercoles";
+        break;
+
+    case 4:
+        echo "es jueves";
+        break;
+
+    case 5:
+        echo "es viernes";
+        break;
+
+    case 6:
+        echo "es sabado";
+        break;
+
+    case 7:
+        echo "es domindo";
+        break;
+
+    default: 
+        echo "no existe ese dia";
+}
